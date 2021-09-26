b78e8866cc7bba7cb6d2a152fccec672f63f4da4
Kaleb Dykema <dykemakaleb@gmail.com>
2021-09-25 22:19:31 -0500

    Added SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './styles.scss'
+
 const App = () => (
   <div>
     <h1>Hello world</h1>
     <h3>From inside ./app.tsx, which is bundled to public/bundle.js</h3>
   </div>
 )
-
-console.log(document.getElementById('root'))
 
 ReactDOM.render(<App/>, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,18 @@ module.exports = {
           loader: 'ts-loader',
           test: /\.(ts|tsx)$/,
           exclude: /node_modules/
-        }
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
       ]
   },
   devServer: {
